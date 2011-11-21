07268f8c186fbaaf2d7185e843ddf35532e5cb7e
Jose Cano <ppcanodehuelva@gmail.com>
2011-11-21 16:42:47 +0100

    adding configuration

---
+++ config/spotify_config.rb
@@ -0,0 +1,7 @@
+
+configure do
+
+    set :spotify_username => ENV['SPOTIFY_USERNAME']
+    set :spotify_password => ENV['SPOTIFY_PASSWORD']
+    
+end
--- playlist.rb
+++ playlist.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
+require 'hallon'
+require './config/spotify_config'
 
+puts "Successfully logged in!"
+
+session = Hallon::Session.initialize IO.read('./config/spotify_appkey.key') do
+  on(:log_message) do |message|
+    puts "[LOG] #{message}"
+  end
+
+  on(:connection_error) do |error|
+    Hallon::Error.maybe_raise(error)
+  end
+
+  on(:logged_out) do
+    abort "[FAIL] Logged out!"
+  end
+end
+
+session.login!(settings.spotify_username, settings.spotify_password)
 
 get '/hi' do
-  "Hello pepe World"
+  settings.pepe
 end
+
+# TODO: using heroku logs
