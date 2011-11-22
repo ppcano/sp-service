b0737cb53d119b25e5330b980acc5d16df006e2f
Jose Cano <ppcanodehuelva@gmail.com>
2011-11-22 17:07:30 +0100

    fix

--- app.rb
+++ app.rb
@@ -44,38 +44,33 @@ get '/playlist/:playlisturi' do
 
   begin
 
-  playlist = Hallon::Playlist.new(params[:playlisturi])
-  session.wait_for { playlist.loaded? }
-  
-  #http://yehudakatz.com/2010/05/05/ruby-1-9-encodings-a-primer-and-the-solution-for-rails/
-  js_tracks = Array.new
-  playlist.tracks.each_with_index do |track, i|
+    playlist = Hallon::Playlist.new(params[:playlisturi])
+    session.wait_for { playlist.loaded? }
+    
+    #http://yehudakatz.com/2010/05/05/ruby-1-9-encodings-a-primer-and-the-solution-for-rails/
+    js_tracks = Array.new
+    playlist.tracks.each_with_index do |track, i|
 
-        session.wait_for { track.loaded? }
+          session.wait_for { track.loaded? }
 
-        album = track.album
-        session.wait_for { album.loaded? }
-        
-        #Spotify::Pointer address=0x00000000000000> is not a valid spotify link URI or pointer
-        #js_album = {:title=> album.name, :image=> album.cover(false).to_str }    
-        js_album = { :title=> album.name.force_encoding("UTF-8") }    
-
-        js_artists = Array.new
-        track.artists.each_with_index do |artist, j|
-          session.wait_for { artist.loaded? }
+          album = track.album
+          session.wait_for { album.loaded? }
           
-          js_artists.push( { :title=> artist.name.force_encoding("UTF-8")} )
+          #Spotify::Pointer address=0x00000000000000> is not a valid spotify link URI or pointer
+          #js_album = {:title=> album.name, :image=> album.cover(false).to_str }    
+          js_album = { :title=> album.name.force_encoding("UTF-8") }    
 
-        end
+          js_artists = Array.new
+          track.artists.each_with_index do |artist, j|
+            session.wait_for { artist.loaded? }
+            
+            js_artists.push( { :title=> artist.name.force_encoding("UTF-8")} )
 
-       js_tracks.push( { :title=> track.name.force_encoding("UTF-8"), :uri=> track.to_link.to_str, :album=> js_album, :artists=>js_artists} )
+          end
 
-  end
+         js_tracks.push( { :title=> track.name.force_encoding("UTF-8"), :uri=> track.to_link.to_str, :album=> js_album, :artists=>js_artists} )
 
-    #{:uri=> playlist.to_link.to_str, :title=> playlist.name.force_encoding("UTF-8"), :tracks => js_tracks}.to_json
-    #{  :title=> playlist.name }.to_json
-  
-    puts 'ending.............................here...'
+    end
 
   rescue => e
      puts e.message
